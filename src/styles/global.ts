60f4a70f0c8e841322f46a3c6fefbf326106ef65
bruna_magalhaes <bruna.magalhaes@hdn.digital>
2023-06-24 17:46:41 -0300

    feat: create global styles and default theme

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle<{ $whiteColor?: boolean }>`
+* {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+}
+:focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme["yellow-500"]};
+}
+body{
+    background-color: ${({ theme }) => theme["gray-600"]};
+    color: ${({ theme }) => theme["gray-300"]};
+}
+body, input-security, button {
+    font: 400 1rem "Roboto", sans-serif;
+}
+`;
