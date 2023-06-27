640722bbd99a97693c677f8a2c29bcbdc35653e1
bruna_magalhaes <bruna.magalhaes@hdn.digital>
2023-06-27 10:02:46 -0300

    feat: create style to header content

--- src/components/base/Header/styles.ts
+++ src/components/base/Header/styles.ts
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``;
+export const HeaderContainer = styled.header`
+  background-color: ${({ theme }) => theme["gray-600"]};
+  padding: 2.5rem 0 7.5rem;
 
-export const HeaderContent = styled.div``;
+  grid-column-start: 2;
+  grid-column-end: 3;
+  display: grid;
+  grid-template-columns: 1fr 70rem 7rem 1fr;
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 70rem;
+
+  grid-column: 2;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
