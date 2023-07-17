056746fa178ad8cb67e60134a11c3606e3ed0fd5
Bruna Magalhães Criscuolo <bruna.magalhaes@levva.io>
2023-07-17 18:34:58 -0300

    feat: create new category dialog

--- src/components/base/DialogNewCategory/index.tsx
+++ src/components/base/DialogNewCategory/index.tsx
@@ -3,6 +3,8 @@ import { ReactNode } from 'react';
 
 import { Button } from "../../forms/Button";
 import { Dialog } from '../Dialog';
+import { Input } from '../../forms/Input';
+import { Form } from '../../forms/Form';
 
 export function DialogNewCategory(): JSX.Element {
   const newCategoryButton: ReactNode = (
@@ -13,7 +15,13 @@ export function DialogNewCategory(): JSX.Element {
 
   return (
     <Dialog title='Nova Categoria' trigger={newCategoryButton}>
-      <p>Isso é um teste</p>
+      <Form>
+        <Input placeholder="Descrição" type="text" required />
+
+        <Button type="submit" variant="primary-form">
+          Cadastrar
+        </Button>
+      </Form>
     </Dialog>
   );
 }
